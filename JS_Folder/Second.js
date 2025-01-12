fc4c41c47c8043efc4c61557d4cd467a599fa3f6
Rabindra Mishra <sambhu311275@gmail.com>
2025-01-12 16:17:15 +0530

    Projects and JS

---
+++ JS_Folder/Second.js
@@ -0,0 +1,17 @@
+//Declaring a variable
+let account_Ballance=200;
+console.log(account_Ballance);
+console.log(typeof(account_Ballance));
+//String
+let str="Rabindra Mishra";
+console.log(str,typeof(str));
+//Boolean
+let fact=true;
+console.log(typeof(fact));
+//Undefined (When we do not define any value to a variable)
+let unknown;
+console.log(typeof(unknown));
+
+//Max and min safe Integer
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
---
+++ JS_Folder/first.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+let num=20;
+console.log(num);
+let name="Rabindra Mishra";
+console.log(name);
+num=30;
+console.log(num);
+const id=5;
+console.log(id);
+// const id=8; this re declaration is not allowed.beacuse const type.
+// console.log(id);
+
