6d03cee5721285fe4df5a23402a39d045f96a68e
Rabindra Mishra <sambhu311275@gmail.com>
2025-01-12 22:17:08 +0530

    Non primitive Data Types

--- JS_Folder/Third.js
+++ JS_Folder/Third.js
@@ -23,3 +23,36 @@ let fun = function(){
     console.log("Hello welcome to Javascript.")
 }
 fun(); //Function Call.
+
+
+//-----------------------------------------Type Conversion-----------------------------------------------------
+
+let account_Ballance="100";  //But not every string gets converted into number eg:"100y".
+let num=Number(account_Ballance);
+console.log(num,typeof(num));
+
+let is_Loggedin=true
+let n=Number(is_Loggedin);
+console.log(n,typeof(n)); //Output is 1
+
+let x1=null;
+console.log(Number(x1)); //output is 0 (null)
+
+let x2; //Convering NaN
+console.log(Number(x2))//Output is NaN.
+
+//****************************String************************************
+let x3=25;
+console.log(String(x3))
+
+let x4=false;
+console.log(String(x4))
+
+//*********** Boolean**************
+let x5=""; //even for space it would provide output as true. beacuase space is also a character.
+console.log(Boolean(x5)); //false 
+
+let x6=34;
+console.log(Boolean(x6));
+
+//********** HW (Bigint) //
