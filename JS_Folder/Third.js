eacd60621124755df8ebebfc58cc20de5548c3d7
Rabindra Mishra <sambhu311275@gmail.com>
2025-01-12 16:37:20 +0530

    Non primitive Data type

---
+++ JS_Folder/Third.js
@@ -0,0 +1,18 @@
+//Non primitive Data Types
+//Array,Object,Function
+
+let arr =[10,44,"Raun",false] //Can accept any type of data unlike c/c++.
+console.log(arr) //Typeof arr is object.
+console.log(typeof arr)
+
+
+
+
+//Object Datatype
+//Uses Key Value pairs
+let obj = {
+user_name:"Rabindra",
+acc_number:56,
+balance:420
+}
+console.log(obj,typeof(obj))
