9a0bc189f8c0ba71b9ceb5bb2c73d5e9a01b82e6
Rabindra Mishra <sambhu311275@gmail.com>
2025-01-14 23:56:35 +0530

    Completed Fourth.js

--- JS_Folder/Fourth.js
+++ JS_Folder/Fourth.js
@@ -1,6 +1,24 @@
 //Operators in Javascript
-// Arithmatic Operators +,-,/,*
+// Arithmatic Operators +,-,/,*,%(Modulus)
 // Priority in JS Operator  1)Divide Multiply {Left to Right} 2) Add Subtract {Left to Right}
 console.log(6*3 + (18/3) - 9 );   //output : 15 {18 + 6 -9} 
 
 //Note using brackets are considered good practice.
+
+console.log(20%3) //Provides Remainder.
+
+//++ Increement, -- Decreement Operator.
+let sum =20;
+console.log(++sum);  //Pehle increement hoga then print hoga.
+total=sum++ //IN this case total is storing the old value of sum and then value of sum is being updated.
+console.log(sum);
+
+
+//Assignment Operator
+let x=10
+x+=10
+console.log(x) //Output:- 20
+
+let y=30
+y/=3
+console.log(y) //output:-10
