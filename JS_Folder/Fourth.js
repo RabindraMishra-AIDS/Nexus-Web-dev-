5b321a4f8fd4c06edc54bfe0cd1d24e93a24065e
Rabindra Mishra <sambhu311275@gmail.com>
2025-01-14 23:37:15 +0530

    Created Fourth.js

---
+++ JS_Folder/Fourth.js
@@ -0,0 +1,6 @@
+//Operators in Javascript
+// Arithmatic Operators +,-,/,*
+// Priority in JS Operator  1)Divide Multiply {Left to Right} 2) Add Subtract {Left to Right}
+console.log(6*3 + (18/3) - 9 );   //output : 15 {18 + 6 -9} 
+
+//Note using brackets are considered good practice.
