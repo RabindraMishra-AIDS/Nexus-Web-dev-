534ca68672deca3d8e73db33ffdc55473c20427c
Rabindra Mishra <sambhu311275@gmail.com>
2025-01-26 01:19:55 +0530

    Updated Memory.js

--- JS_Folder/Memory.js
+++ JS_Folder/Memory.js
@@ -7,3 +7,14 @@ let obj1={
 let obj2=obj1;
 obj2.id=2;
 console.log(obj1)  //Changes in obj1 will be reflected.(Non Primitive)
+
+
+/* **************** const in Non Primitive ********************************* */
+const obj={
+    id:1,
+    name:'rabindra'
+}
+obj.id=2;
+console.log(obj)  //Here it will not throw error due to const because reference is not changing.
+
+obj=obj1 //It will throw error since we are passing address of obj1 in obj(const) ie.chnaging refrence. 
